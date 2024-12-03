0c0a93b571ae460c11d4bcd7212a46bfb405d2a6
Jeremy Bryant <jb@jeremybryant.net>
2024-12-03 22:39:32 +0000

    Initial commit

---
+++ c-intro-and-ref-pkg.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "c-intro-and-ref" "20241201"
+  "GNU C Language Introduction and Reference Manual")
+;;  :maintainers '(("Jeremy Bryant" . "jb@jeremybryant.net")))
---
+++ c-intro-and-ref.el
@@ -0,0 +1,63 @@
+;;; c-intro-and-ref.el --- GNU C Language Introduction and Reference Manual -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Jeremy Bryant
+
+;; Author: Jeremy Bryant <jb@jeremybryant.net>
+;; Maintainer: Jeremy Bryant <jb@jeremybryant.net>
+;; Version: 20241201
+;; Website: https://www.gnu.org/software/c-intro-and-ref/
+;; Savannah: https://savannah.gnu.org/projects/c-intro-and-ref/
+
+;; Keywords: docs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package is a distribution of
+;; `The GNU C Language Introduction and Reference manual',
+;; written by Richard Stallman, et al.
+
+;; The original manual c.texi, included is in the TeXinfo format.
+;; This package installs the info manual for reference within Emacs.
+;; Upon installation, it is available under C-h i.
+
+;; This package also contains PDF version.
+
+;; An excerpt from the beginning of the manual is below
+
+;; GNU C Manual
+;; ************
+
+;; This manual explains the C language for use with the GNU Compiler
+;; Collection (GCC) on the GNU/Linux operating system and other systems.
+;; We refer to this dialect as GNU C. If you already know C, you can use
+;; this as a reference manual.
+
+;; If you understand basic concepts of programming but know nothing
+;; about C, you can read this manual sequentially from the beginning to
+;; learn the C language.
+
+;; License of the Manual
+
+;; GNU C Language Introduction and Reference Manual is free documentation; you can redistribute it and/or modify it under the terms of the GNU Free Documentation License as published by the Free Software Foundation; either version 1.3 of the License, or (at your option) any later version published by the Free Software Foundation; with with the Invariant Sections being “GNU General Public License,” with the Front-Cover Texts being “A GNU Manual,” and with the Back-Cover Texts as in (a) below.  A copy of the license is included in the section entitled “GNU Free Documentation License.”
+
+;; (a) The FSF's Back-Cover Text is: “You have the freedom to copy and
+;; modify this GNU manual.”
+
+;; Please report errors and suggestions to c-manual@@gnu.org.
+
+;;; Code:
+
+;;; c-intro-and-ref.el ends here
